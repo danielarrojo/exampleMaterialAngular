f6b7846b426e00a9233e246b1740d2f1250781fa
Daniel Arrojo Onrubia <danielarrojo@gmail.com>
2020-12-01 12:37:51 +0100

    Nociones FlexLayout

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, ModuleWithProviders, Type } from '@angular/core';
+
+const MaterialComponents: any[] | Type<any> | ModuleWithProviders<{}> = [];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
