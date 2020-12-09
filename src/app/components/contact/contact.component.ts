a9ffbae4122f7e3915c0db36958366b1974f2173
Daniel Arrojo Onrubia <danielarrojo@gmail.com>
2020-12-09 11:47:38 +0100

    MatForm - Step 2 - Incomplete

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -8,6 +8,7 @@ import { FormGroup, NgForm, FormControl, Validators} from '@angular/forms'
 })
 export class ContactComponent implements OnInit {
   contactForm: FormGroup;
+  patternEmail: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor() { 
     this.contactForm = this.createFormGroup();
@@ -24,7 +25,10 @@ export class ContactComponent implements OnInit {
     return new FormGroup({
       firstName : new FormControl('', [Validators.required, Validators.minLength(5)]),
       lastName : new FormControl('', [Validators.required, Validators.minLength(5)]),
-      address : new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)])
+      address : new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+      email : new FormControl('', [Validators.required, Validators.pattern(this.patternEmail)]),
+      birthDate : new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required])
     });
   }
 
